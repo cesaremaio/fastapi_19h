f02900aefb5d3748b72826c992530eb8d4170f53
cesare <cesare.maio@italdesign.it>
2025-03-18 08:39:15 +0000

    second commit

--- app/utils.py
+++ app/utils.py
@@ -10,4 +10,4 @@ def hash(password: str):
 
 
 def verify(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password, hashed_password) 
